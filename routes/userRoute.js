80a0bdadccd1ed43ab540b959d4c9e7cc594ebf5
Leandro-SI <llmanutencao@bol.com.br>
2022-03-08 13:22:58 -0300

    rota para listar usuarios

---
+++ routes/userRoute.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const { join } = require('path')
+
+const filePath = join(__dirname, 'users.json')
+
+// ler usuários do arquivo .json
+const getUsers = () => {
+    const data = fs.existsSync(filePath) ? fs.readFileSync(filePath) : []
+
+    try {
+        return JSON.parse(data)
+    } catch (error) {
+        
+    }
+}
+
+// salvar usuários
+const saveUser = (users) => fs.writeFileSync(filePath.JSON.stringify(users, null, '\t'))
+
+const userRoute = (app) => {
+    app.route('/users/:id?')
+        .get((req, res) => {
+            const users = getUsers()
+
+            res.send({ users })
+        })
+}
+
+module.exports = userRoute
